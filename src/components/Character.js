b1499b1b15801a966ad98e28a69c6b90901e8ae9
JavachipMom <Nicolesp4ever@gmail.com>
2020-09-28 18:45:12 -0500

    Rest of my MVP changes..

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,21 +1,38 @@
-import React from "react";
-import { Card, CardTitle, CardText, CardDeck, CardBody } from "reactstrap";
-
 // Write your Character component here
+import React from "react";
+import { Card, CardTitle, CardText, CardBody } from "reactstrap";
+import styled from "styled-components";
 
 const Character = ({ individual }) => {
+  const CardDeck = styled.div`
+    background: white;
+    display: flex;
+    flex-direction: column;
+    align-items: auto;
+    justify-content: space-between;
+    width: 500px;
+    margin: auto;
+    border-style: dashed;
+  `;
+
+  const style = {
+    color: individual.gender === "male" ? "royalblue" : "pink",
+  };
+
   return (
-    <CardDeck>
-      <Card key={individual.id}>
-        <CardBody>
-          <CardTitle tag="h2">{individual.name}</CardTitle>
-          <CardText>{individual.gender}</CardText>
-          <CardText>Birth Year: {individual.birth_year}</CardText>
-          <CardText>Skin Color: {individual.skin_color}</CardText>
-          <CardText></CardText>
-        </CardBody>
-      </Card>
-    </CardDeck>
+    <div key={individual.id}>
+      <CardDeck>
+        <Card>
+          <CardBody>
+            <CardTitle tag="h2">{individual.name}</CardTitle>
+            <CardText style={style}>{individual.gender}</CardText>
+            <CardText>Birth Year: {individual.birth_year}</CardText>
+            <CardText>Skin Color: {individual.skin_color}</CardText>
+            <CardText></CardText>
+          </CardBody>
+        </Card>
+      </CardDeck>
+    </div>
   );
 };
 
